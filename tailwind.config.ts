8130d53846ae4d7dcacfeb57dead205b012ebe96
danielpsrb <danielpsrb01@gmail.com>
2024-07-10 01:31:43 +0700

    add theme from daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,5 +16,8 @@ const config: Config = {
     },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "synthwave", "coffee", "halloween", "forest", "emerald", "corporate", "cyberpunk"],
+  },
 };
 export default config;
