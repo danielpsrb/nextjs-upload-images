bb8122911f604ba9bd4f3b806d6629c3441db8d8
danielpsrb <danielpsrb01@gmail.com>
2024-07-10 01:32:25 +0700

    update next config add hostname blob vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+        {
+            protocol: "https",
+            hostname: "2n57rx43bzyb76dy.public.blob.vercel-storage.com",
+        },
+        ],
+    },
+};
 
 export default nextConfig;
