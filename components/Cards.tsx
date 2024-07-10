56474d498d85680c9bd13b44a3476dbbe962f2b3
danielpsrb <danielpsrb01@gmail.com>
2024-07-11 02:46:30 +0700

    update cards components style

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -1,21 +1,28 @@
 import Image from "next/image";
+import { EditButton, DeleteButton } from "./Buttons";
 import type { Images } from "@prisma/client";
 
 const Cards = ({ data }: { data: Images }) => {
     return (
-        <div className="card bg-base-300 w-96 shadow-xl">
-            <figure className="px-2 pt-2">
+        <div className="max-w-sm border border-[#7FFF00] rounded-md shadow">
+            <figure className="relative aspect-video">
                 <Image
                     src={data.image_url}
                     alt={data.title}
-                    width={900}
-                    height={900}
-                    className="rounded-lg"
+                    fill
+                    priority
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    className="rounded-t-md object-cover"
                 />
             </figure>
-            <div className="card-body items-center text-center">
+            <div className="p-5">
                 <h2 className="card-title">{data.title}</h2>
                 <p>{data.description}</p>
+            </div>
+
+            <div className="flex items-center justify-between">
+                <EditButton id={data.id} />
+                <DeleteButton id={data.id} />
             </div>
         </div>
     );
